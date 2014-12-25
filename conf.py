6a9a7734a8cf8d014e4a906cca9c98f6e3f60e2b
feng-pu yang <fengpuyang@gmail.com>
2014-12-25 14:06:37 +0800

    add http highlight configuration
    + sphinxcontrib.httpdomain

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.phpdomain']
+extensions = ['sphinxcontrib.phpdomain','sphinxcontrib.httpdomain']
 
 # The name of the default domain.
 primary_domain = 'php'
